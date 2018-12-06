0e121eaf84678e6dd22f66cfb15c64d031b1ff4b
Rasl-Ait <aitekov_rasul@mail.ru>
2018-12-06 08:26:02 +0300

    Initial Commit

---
+++ MyFirebaseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirebaseApp
+//
+//  Created by rasl on 06.12.2018.
+//  Copyright © 2018 rasl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
